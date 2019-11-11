6cc3027a6e620e495db8fdb476530dd4d23d5b5f
David Adrian <davidcadrian@gmail.com>
2019-11-11 14:17:46 -0500

    Add debug logging option

--- where.go
+++ where.go
@@ -4,10 +4,11 @@ import (
 	"encoding/json"
 	"flag"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"strings"
 	"time"
+
+	log "github.com/sirupsen/logrus"
 
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2/google"
@@ -36,21 +37,35 @@ func GetTimeFromEventTime(e *calendar.EventDateTime) (time.Time, error) {
 	}
 }
 
+type ServiceAccountKey struct {
+	ClientEmail string `json:"client_email,omitempty"`
+}
+
 func main() {
 
 	var cid string
 	var keyPath string
 	var defaultLocation string
+	var verbose bool
 	flag.StringVar(&cid, "calendar", "primary", "Zakir travel calendar ID")
 	flag.StringVar(&keyPath, "key", "where-is-zakir-key.json", "path to JSON key")
 	flag.StringVar(&defaultLocation, "default-location", "Unknown", "displays when nothing is listed on the calendar")
+	flag.BoolVar(&verbose, "verbose", false, "debug-level logging")
 	flag.Parse()
+
+	if verbose {
+		log.SetLevel(log.DebugLevel)
+	}
 
 	ctx := context.Background()
 
 	b, err := ioutil.ReadFile(keyPath)
 	if err != nil {
 		log.Fatalf("Unable to read client secret file: %s", err)
+	}
+	var k ServiceAccountKey
+	if err := json.Unmarshal(b, &k); err != nil {
+		log.Fatalf("Could not deserialize key json: %s", err)
 	}
 
 	config, err := google.JWTConfigFromJSON(b, calendar.CalendarReadonlyScope)
@@ -94,6 +109,7 @@ func where(srv *calendar.Service, cid string, cache *zcache) string {
 	expiry := cache.When.Add(time.Minute)
 
 	if n.Before(expiry) {
+		log.Debugf("using cache (expiry %d)", expiry)
 		return cache.Location
 	}
 
@@ -104,25 +120,35 @@ func where(srv *calendar.Service, cid string, cache *zcache) string {
 	current := ""
 
 	if err == nil && len(events.Items) > 0 {
+		log.Debugf("got %d events", len(events.Items))
 		for _, i := range events.Items {
+			log.Debugf("checking event \"%s\"", i.Summary)
 			start, err := GetTimeFromEventTime(i.Start)
 			if err != nil {
 				log.Fatalf("could not get start time: %v", err)
 			}
+			log.Debugf("start: %d", start)
 			end, err := GetTimeFromEventTime(i.End)
 			if err != nil {
 				log.Fatalf("could not get end time: %v", err)
 			}
+			log.Debugf("end: %d", end)
 			parts := strings.Split(i.Summary, "(")
 			name := parts[0]
 			name = strings.Trim(name, " ")
 			if start.Before(n) && end.After(n) {
+				log.Debug("using event as current!")
 				current = name
 			}
 		}
+	} else if err != nil {
+		log.Errorf("error talking to calendar: %s", err)
+	} else {
+		log.Debug("no events received")
 	}
 
 	if current == "" {
+		log.Debugf("couldn't find event, using default %s", cache.DefaultLocation)
 		current = cache.DefaultLocation
 	}
 	cache.Location = current
